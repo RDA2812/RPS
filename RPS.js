db4479895d30ea84d145d132e7580adf8f2973cd
Ross Allan <ross.allan1@hotmail.com>
2020-02-18 21:36:16 +0000

    started over with actual UI

---
+++ RPS.js
@@ -0,0 +1,12 @@
+
+    var possibles = ["Rock", "Paper", "Scissors"];
+    var computerSelection =  possibles[Math.floor(Math.random() * possibles.length)];
+    var playerSelection = //prompt("Rock, Paper or Scissors?")  ;
+    var result = "";
+    var playerScore = 0;
+    var computerScore = 0;
+
+
+    console.log(computerSelection, playerSelection);
+
+  
